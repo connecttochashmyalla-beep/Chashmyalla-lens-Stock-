5c15d8a8dbebbcbb3108b773c6b887920edb93c9
connecttochashmyalla-beep <connecttochashmyalla@gmail.com>
2026-07-22 18:18:26 +0500

    Refactor PostCSS config to use CommonJS format

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  root: '.',
-  plugins: [react()],
-  server: {
-    port: 3000,
-    strictPort: false
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      input: './index.html'
-    }
-  }
-})
+}
